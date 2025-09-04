e38455746aa735689d978171af06a16756419427
Nadya <224967973+nadmak11@users.noreply.github.com>
2025-09-04 15:23:24 +0300

    внесена строка 2 "#Изменение файла"

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 # setup.py
+# Изменение файла
 from setuptools import setup, find_packages
 
 setup(
-    name="shop_package",          # Имя пакета для pip
-    version="0.1.0",              # Версия
+    name="shop_package",  # Имя пакета для pip
+    version="0.1.0",  # Версия
     author="Твоё Имя",
     description="Простой пакет для имитации работы интернет-магазина",
-    packages=find_packages(),     # Автоматически найти все пакеты (shop, shop.data и т.д.)
-    install_requires=[            # Список зависимостей, как в requirements.txt
+    packages=find_packages(),  # Автоматически найти все пакеты (shop, shop.data и т.д.)
+    install_requires=[  # Список зависимостей, как в requirements.txt
         "requests",
     ],
-    python_requires=">=3.7",      # Требуемая версия Python
+    python_requires=">=3.7",  # Требуемая версия Python
 )
